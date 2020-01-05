67a86fdc2fe622279741d4baf45529087974f0e6
yulineko <yulineko@gmail.com>
2020-01-05 18:43:19 -0500

    functions

--- Udemy-Exercises/functinos.py
+++ Udemy-Exercises/functinos.py
@@ -68,3 +68,61 @@ def single_letter_count(string,letter):
 # 11
 def multiple_letter_count(string):
     return {letter: string.count(letter) for letter in string}
+
+# 12
+def list_manipulation(collection, command, location, value=None):
+    if(command == "remove" and location == "end"):
+        return collection.pop()
+    elif(command == "remove" and location == "beginning"):
+        return collection.pop(0)
+    elif(command == "add" and location == "beginning"):
+        collection.insert(0,value)
+        return collection
+    elif(command == "add" and location == "end"):
+        collection.append(value)
+        return collection
+
+# 13
+def is_palindrome(string):
+    stripped = string.replace(" ", "")
+    return stripped == stripped[::-1]
+
+# 14
+def frequency(collection, searchTerm):
+    return collection.count(searchTerm)
+
+# 15
+def multiply_even_numbers(lst):
+    total = 1
+    for val in lst:
+        if val % 2 == 0:
+            total = total * val
+    return total
+
+# 16
+def capitalize(string):
+    return string[:1].upper() + string[1:]
+
+# 17
+def compact(l):
+    return [val for val in l if val]
+
+# 18
+def intersection(l1, l2):
+    in_common = []
+    for val in l1:
+        if val in l2:
+            in_common.append(val)
+    return in_common
+
+# 19
+def partition(lst, fn):
+    trues = []
+    falses = []
+    for val in lst:
+        if fn(val):
+            trues.append(val)
+        else:
+            falses.append(val)
+    return [trues, falses]
+
