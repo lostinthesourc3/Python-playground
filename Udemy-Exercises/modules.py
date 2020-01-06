73fac7880409fe7d7ec8db2a69b35cba916c988e
yulineko <yulineko@gmail.com>
2020-01-05 19:00:55 -0500

    modules

---
+++ Udemy-Exercises/modules.py
@@ -0,0 +1,28 @@
+# modules exercises 
+# 1
+import math
+ 
+answer = math.sqrt(15129)
+
+# 2
+import keyword
+ 
+def contains_keyword(*args):
+    for item in args:
+        if keyword.iskeyword(item): return True
+    return False
+
+# 3
+# helpers.py: 
+def lucky_number(): 
+    return 37
+
+import helpers
+num = helpers.lucky_number()
+
+# 4
+
+
+
+
+
