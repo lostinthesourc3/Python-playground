8267c3bdb7fef6f3b727bebcb2fa425daa257803
yulineko <yulineko@gmail.com>
2020-02-02 20:16:31 -0500

    OOP

--- Udemy-Exercises/oop-intro.py
+++ Udemy-Exercises/oop-intro.py
@@ -42,3 +42,36 @@ class Chicken:
         self.eggs += 1
         Chicken.total_eggs += 1
         return self.eggs
+
+# 5
+class Character():
+    def __init__(self, name, hp, level):
+        self.name = name
+        self.hp = hp
+        self.level = level
+
+class NPC(Character):
+    def __init__(self, name, hp, level):
+        super().__init__(name, hp, level)
+ 
+    def speak(self):
+        return "{0} says: 'I heard monsters running around last night!'".format(self.name)
+
+# 6
+class Mother:
+    def __init__(self):
+        self.eye_color = "brown"
+        self.hair_color = "dark brown"
+        self.hair_type = "curly"
+ 
+ 
+class Father:
+    def __init__(self):
+        self.eye_color = "blue"
+        self.hair_color = "blond"
+        self.hair_type = "straight"
+ 
+ 
+class Child(Mother, Father):
+    pass
+
