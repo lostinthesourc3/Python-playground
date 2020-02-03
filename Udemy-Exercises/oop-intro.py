bbd10603a8e7965dca2860c33d81baef1f339175
yulineko <yulineko@gmail.com>
2020-02-02 20:17:07 -0500

    OOP

--- Udemy-Exercises/oop-intro.py
+++ Udemy-Exercises/oop-intro.py
@@ -75,3 +75,13 @@ class Father:
 class Child(Mother, Father):
     pass
 
+#  7
+class Train():
+    def __init__(self, num_cars):
+        self.num_cars = num_cars
+ 
+    def __repr__(self):
+        return "{} car train".format(self.num_cars)
+ 
+    def __len__(self):
+        return self.num_cars
