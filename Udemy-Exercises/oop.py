3ad6357f7a07d93bd7132003c5ce90a4926253cd
yulineko <yulineko@gmail.com>
2020-03-01 18:49:09 -0500

    ex 2

--- Udemy-Exercises/oop.py
+++ Udemy-Exercises/oop.py
@@ -13,3 +13,21 @@ class NPC(Character):
     def speak(self):
         return "{0} says: 'I heard monsters running around last night!'".format(self.name)
 
+# 2
+class Mother:
+    def __init__(self):
+        self.eye_color = "brown"
+        self.hair_color = "dark brown"
+        self.hair_type = "curly"
+ 
+ 
+class Father:
+    def __init__(self):
+        self.eye_color = "blue"
+        self.hair_color = "blond"
+        self.hair_type = "straight"
+ 
+ 
+class Child(Mother, Father):
+    pass
+
