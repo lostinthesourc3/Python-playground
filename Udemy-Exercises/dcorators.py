573dd70badbd814fd5aa75c785b2cb225da111a3
yulineko <yulineko@gmail.com>
2020-03-01 19:04:33 -0500

    decorators 4

--- Udemy-Exercises/dcorators.py
+++ Udemy-Exercises/dcorators.py
@@ -31,3 +31,14 @@ def ensure_fewer_than_three_args(fn):
         return "Too many arguments!"
     return wrapper
 
+# 4
+from functools import wraps
+ 
+def only_ints(fn):
+    @wraps(fn)
+    def inner(*args, **kwargs):
+        if any([arg for arg in args if type(arg) != int]):
+            return "Please only invoke with integers."
+        return fn(*args, **kwargs)
+    return inner
+
