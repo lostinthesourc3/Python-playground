d18d2a4de385b4b4ab042660830a4c86c605b5e2
yulineko <yulineko@gmail.com>
2020-03-01 19:02:14 -0500

    decorators 3

--- Udemy-Exercises/dcorators.py
+++ Udemy-Exercises/dcorators.py
@@ -20,4 +20,14 @@ def double_return(fn):
         return [val, val]
     return wrapper
 
+# 3
+from functools import wraps
+ 
+def ensure_fewer_than_three_args(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if len(args) < 3:
+            return fn(*args, **kwargs)
+        return "Too many arguments!"
+    return wrapper
 
