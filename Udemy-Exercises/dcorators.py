5a2d0fa49a15bfb2b75940accb889c6ddf1f3c71
yulineko <yulineko@gmail.com>
2020-03-01 19:01:07 -0500

    decorators 2

--- Udemy-Exercises/dcorators.py
+++ Udemy-Exercises/dcorators.py
@@ -10,3 +10,14 @@ def show_args(fn):
         return fn(*args, **kwargs)
     return wrapper
 
+# 2
+from functools import wraps
+ 
+def double_return(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        val = fn(*args, **kwargs)
+        return [val, val]
+    return wrapper
+
+
