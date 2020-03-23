431882f91cb2ec144f5cfe1451fc5c4d844e67c8
yulineko <yulineko@gmail.com>
2020-03-22 22:09:47 -0400

    file.io ex

--- Udemy-Exercises/fileio.py
+++ Udemy-Exercises/fileio.py
@@ -82,3 +82,23 @@ def update_users(old_first, old_last, new_first, new_last):
                 csv_writer.writerow(row)
  
     return "Users updated: {}.".format(count)
+
+
+# 9
+import csv
+ 
+def delete_users(first_name, last_name):
+    with open("users.csv") as csvfile:
+        csv_reader = csv.reader(csvfile)
+        rows = list(csv_reader)
+ 
+    count = 0
+    with open("users.csv", "w") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        for row in rows:
+            if row[0] == first_name and row[1] == last_name:
+                count += 1
+            else:
+                csv_writer.writerow(row)
+ 
+    return "Users deleted: {}.".format(count)
