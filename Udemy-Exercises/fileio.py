d929f2ee944543909d539fa8e2873c76e0c70eb7
yulineko <yulineko@gmail.com>
2020-03-22 22:08:00 -0400

    file.io ex

--- Udemy-Exercises/fileio.py
+++ Udemy-Exercises/fileio.py
@@ -32,3 +32,36 @@ def find_and_replace(file_name, old_word, new_word):
         file.write(new_text)
         file.truncate()
 
+# working with csv
+# 5
+import csv
+ 
+def add_user(first_name, last_name):
+    with open("users.csv", "a") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow([first_name, last_name])
+
+# 6
+import csv
+ 
+def print_users():
+    with open("users.csv") as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        for row in csv_reader: 
+            print("{} {}".format(row['First Name'], row['Last Name']))
+
+# 7
+import csv
+ 
+def find_user(first_name, last_name):
+    with open("users.csv") as csvfile:
+        csv_reader = csv.reader(csvfile)
+        for (index, row) in enumerate(csv_reader):
+            first_name_match = first_name == row[0]
+            last_name_match = last_name == row[1]
+            if first_name_match and last_name_match:
+                return index
+        return "{} {} not found.".format(first_name, last_name)
+
+
+
