0593357b4ae8aa9fa7d6ef8945348ea273b3b14b
yulineko <yulineko@gmail.com>
2020-03-22 22:06:23 -0400

    file.io ex

--- Udemy-Exercises/fileio.py
+++ Udemy-Exercises/fileio.py
@@ -23,3 +23,12 @@ def statistics(file_name):
              "words": sum(len(line.split(" ")) for line in lines),
              "characters": sum(len(line) for line in lines) }
 
+# 4
+def find_and_replace(file_name, old_word, new_word):
+    with open(file_name, "r+") as file:
+        text = file.read()
+        new_text = text.replace(old_word, new_word)
+        file.seek(0)
+        file.write(new_text)
+        file.truncate()
+
